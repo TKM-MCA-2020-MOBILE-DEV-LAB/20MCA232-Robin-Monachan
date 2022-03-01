db94f632d806538ef0419d4aa332cf65bafcb56f
Robin Monachan <2005@tkmce.ac.in>
2022-03-01 23:12:39 +0530

    CO3 complete

--- CO3/Q3/MainActivity2.java
+++ CO3/Q3/MainActivity2.java
@@ -1,29 +1,41 @@
-package com.example.co3_q3explicitintent;
+package com.example.shared;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    TextView view1;
-    TextView view2;
-    TextView view3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView t1,t2,t3;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        view1 = (TextView) findViewById(R.id.view1);
-        view2 = (TextView) findViewById(R.id.view2);
-        view3 = (TextView) findViewById(R.id.view3);
         Bundle extras = getIntent().getExtras();
-        String value1 = extras.getString("Value1");
-        String value2 = extras.getString("Value2");
-        String value3 = extras.getString("Value3");
-        view1.setText(value1);
-        view2.setText(value2);
-        view3.setText(value3);
+        t1=(TextView) findViewById(R.id.textView1);
+        t2=(TextView) findViewById(R.id.textView2);
+        t3=(TextView) findViewById(R.id.textView3);
+        //t4=(TextView) findViewById(R.id.textView4);
+        //t5=(TextView) findViewById(R.id.textView5);
+
+        String value1 = extras.getString("1");
+        String value2 = extras.getString("2");
+        String value3 = extras.getString("3");
+        t1.setText("Name: "+value1);
+        t2.setText("Address is "+ value3);
+        t3.setText("Email is "+ value2);
+
+
+
+    }
+    public void callFirstActivity(View view){
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
     }
 
 }
--- CO3/Q5/MainActivity.java
+++ CO3/Q5/MainActivity.java
@@ -1,4 +1,6 @@
-package com.example.arrayadapter;
+package com.example.listview;
+
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
